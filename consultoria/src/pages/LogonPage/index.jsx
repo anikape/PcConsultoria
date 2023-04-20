02431829d164704dee9786b790395ec129481bc4
Tati <tanicchetti@gmail.com>
2023-04-20 13:58:33 -0300

    feature: logon simplificado

---
+++ consultoria/src/pages/LogonPage/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FormRegister from "../../components/Forms/FormRegister";
+
+const LogonPage = () => {
+    return (
+        <>
+        <h1>Cadastro</h1>
+        <FormRegister />
+        </>
+    )
+}
+
+export default LogonPage
